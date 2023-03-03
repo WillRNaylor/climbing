78f7fae25d4bff9689b16a1285edcb6184e78f3e
William Naylor <Hermes@MacBook-Pro.local>
2023-03-04 11:41:12 +1300

    moving paths to within folder

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ Runs basic analysis and prints out climbing log.
 
 
 # ---- Input
-LOGFILE = '/Users/Hermes/Dropbox/Climbing_log/climbing_log.txt'
-OUT_FILE = '/Users/Hermes/Dropbox/Climbing_log/climbing_log.md'
+LOGFILE = 'log.txt'
+OUT_FILE = 'README.md'
 
-CRAGFILE = '/Users/Hermes/Dropbox/Climbing_log/Code/reference/crag_index.txt'
-GRADEFILE = '/Users/Hermes/Dropbox/Climbing_log/Code/reference/grade_conv.txt'
+CRAGFILE = './reference/crag_index.txt'
+GRADEFILE = './reference/grade_conv.txt'
 crags = util.get_crag_dict(CRAGFILE)
 grades = util.get_grade_conv(GRADEFILE)
 
